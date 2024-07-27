92ca3d807576b322d6bc816e8fe660ffba8e5a5c
Oscar Andres Macias Narvaez <omaciasnarvaez@gmail.com>
2024-07-27 07:41:37 -0500

    ⚡ feat(soap): Add certificates

--- IIS-Script/Set-AppPool.ps1
+++ IIS-Script/Set-AppPool.ps1
@@ -3,12 +3,4 @@ param (
 	[string]$appPoolName
 )
 
-Write-Output "***********************************"
-Write-Output "** Script developed by Omaciasd. **"
-Write-Output "***********************************"
-Write-Output ""
-
-Write-Output "** Setting application pool for $appName to $appPoolName. **"
 & "$env:SystemRoot\System32\inetsrv\appcmd.exe" set app /app.name:$appName /applicationPool:$appPoolName
-
-Clear-Host
